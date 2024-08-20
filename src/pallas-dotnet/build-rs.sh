63cccd219843dd94b381bcb46635d511b91309ab
Christian Benedict Gantuangco <gantuangcoc98@gmail.com>
2024-08-21 02:41:23 +0800

    refactor(dotnet): changed the build-rs script to generate the wrappers for both n2c and n2n mini protocols

--- src/pallas-dotnet/build-rs.sh
+++ src/pallas-dotnet/build-rs.sh
@@ -2,16 +2,22 @@
 
 # Function to build and copy for Linux
 build_for_linux() {
-    cargo build --release --manifest-path ../pallas-dotnet-rs/Cargo.toml
-    cp ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so "./libpallas_dotnet_rs.so"
-    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so > "./PallasDotnetWrapper.cs"
+    cargo build --release --manifest-path ../pallas-dotnet-rs/n2c-miniprotocols/Cargo.toml
+    cargo build --release --manifest-path ../pallas-dotnet-rs/n2n-miniprotocols/Cargo.toml
+    cp ../pallas-dotnet-rs/n2c-miniprotocols/target/release/libpallas_dotnet_n2c.so "./libpallas_dotnet_n2c.so"
+    cp ../pallas-dotnet-rs/n2n-miniprotocols/target/release/libpallas_dotnet_n2n.so "./libpallas_dotnet_n2n.so"
+    rnet-gen ../pallas-dotnet-rs/n2c-miniprotocols/target/release/libpallas_dotnet_n2c.so > "./NodeToClientWrapper.cs"
+    rnet-gen ../pallas-dotnet-rs/n2n-miniprotocols/target/release/libpallas_dotnet_n2n.so > "./NodeToNodeWrapper.cs"
 }
 
 # Function to build and copy for macOS
 build_for_macos() {
-    cargo build --release --manifest-path ../pallas-dotnet-rs/Cargo.toml
-    cp ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib "./libpallas_dotnet_rs.dylib"
-    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib > "./PallasDotnetWrapper.cs"
+    cargo build --release --manifest-path ../pallas-dotnet-rs/n2c-miniprotocols/Cargo.toml
+    cargo build --release --manifest-path ../pallas-dotnet-rs/n2n-miniprotocols/Cargo.toml
+    cp ../pallas-dotnet-rs/n2c-miniprotocols/target/release/libpallas_dotnet_n2c.dylib "./libpallas_dotnet_n2c.dylib"
+    cp ../pallas-dotnet-rs/n2n-miniprotocols/target/release/libpallas_dotnet_n2n.dylib "./libpallas_dotnet_n2n.dylib"
+    rnet-gen ../pallas-dotnet-rs/n2c-miniprotocols/target/release/libpallas_dotnet_n2c.dylib > "./NodeToClientWrapper.cs"
+    rnet-gen ../pallas-dotnet-rs/n2n-miniprotocols/target/release/libpallas_dotnet_n2n.dylib > "./NodeToNodeWrapper.cs"
 }
 
 # Check the operating system
